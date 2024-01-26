9f65f61b3f903c535d9215cb87d1d540cebdf7b7
YILIN <lyl1784553624@gmail.com>
2024-01-26 10:59:19 +0200

    repository init

---
+++ phpEXERCISES/lec1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- here is a comment -->
+    <?php
+        echo "<h1>hello world! Have a nice day!</h1>";    
+    ?>
+</body>
+</html>
+
