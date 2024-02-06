a40293a2ea31181c8d2dc70c39852668207ca240
Yilin Lai BBCAP23A3 <yilin23000@shell.hamk.fi>
2024-02-06 03:26:08 +0200

    exercise7 on localhost

--- phpTasks/ex5.php
+++ phpTasks/ex5.php
@@ -6,6 +6,9 @@
     Here is the Exercise 5.
 </h1>
 <body>
+    <a class="nav-link" href="https://github.com/YILIN1031/Team24" style="color: black">
+        <b>README.md</b>
+    </a>
 </body>
 <?php
     include 'footer.php';
