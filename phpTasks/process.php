81735de13a7619383d49dc70a4fc6dbf42e54382
YILIN <lyl1784553624@gmail.com>
2024-02-06 09:18:07 +0200

    update exercise7

--- phpTasks/process.php
+++ phpTasks/process.php
@@ -6,8 +6,7 @@
         $city = $_POST['city'];
         $groupid = $_POST['groupid'];
         include 'db.php';
-        $sql = "insert info studentinfo(first_name, last_name, city, groupid) 
-                values('$fname', '$lname', '$city', '$groupid')";
+        $sql = "insert info studentinfo(first_name, last_name, city, groupId) values ('$fname', '$lname', '$city', '$groupid')";
 
         if ($conn -> query($sql) === TRUE)
         {
