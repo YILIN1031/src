f0850d8c8d800a65c62c0a74737e12c03b302387
YILIN <lyl1784553624@gmail.com>
2024-02-06 10:46:48 +0200

    add exercise7, shell.hamk.fi

--- phpTasks/db.php
+++ phpTasks/db.php
@@ -1,8 +1,21 @@
 <?php
+
+// shell.hamk.fi >>> localhost
+// dbname: wp_xxx
+
+/*
+// local machine, docker desktop
 $servername = "web-dev-env-main-db-1";
 $username = "app1";
 $password = "password";
 $dbname = "app1";
+*/
+
+// shell.hamk.fi
+$servername = "localhost";
+$username = "yilin23000";
+$password = "7Y6BNxgK";
+$dbname = "wp_yilin23000";
 
 // create database connection 
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,6 +25,6 @@ if ($conn -> connect_error)
 {
     die("Connection Failed:" . $conn -> connect_error);
 }
-echo "Connected successfully!";
+echo "Connected successfully! ";
 
 ?>
